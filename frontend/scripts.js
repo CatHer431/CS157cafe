3b50c819da7224d79faff8cfc5c32e22944a54ae
cat <chogies02@gmail.com>
2024-04-08 18:38:58 -0700

    Starter Files

---
+++ frontend/scripts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Your React components will go here
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Cafe Management App!</h1>
+      {/* Add more React elements and components here */}
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
